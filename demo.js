e72a53eee107edc226505ed2c4ee006381128217
amitbend <ajbendor@gmail.com>
2016-07-16 23:13:43 +0300

    changed file hierarcy

--- demo.js
+++ demo.js
@@ -12,9 +12,9 @@
       recorderServiceProvider
         .forceSwf(window.location.search.indexOf('forceFlash') > -1)
         .setSwfUrl('lib/recorder.swf')
-        .withMp3Conversion(convert : true, config : {
+        .withMp3Conversion({convert : true, config : {
           bitRate : 320
-        });
+        }});
     });
 
 })();
